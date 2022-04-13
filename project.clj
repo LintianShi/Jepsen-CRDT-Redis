d92fcce21091027aae14cc58ae0829b05c6b8160
LintianShi <254656131@qq.com>
2022-04-13 22:36:45 +0800

    compile jepsen 0.2.7 for local library

--- project.clj
+++ project.clj
@@ -4,6 +4,6 @@
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :main jepsen.crdt-redis
   :dependencies [[org.clojure/clojure "1.10.0"]
-                 [jepsen "0.2.1-SNAPSHOT"]]
+                 [jepsen "0.2.7-LOCAL"]]
   :repositories [["central" "http://maven.aliyun.com/nexus/content/groups/public"]
                  ["clojars" "https://mirrors.tuna.tsinghua.edu.cn/clojars/"]])
--- src/jepsen/crdt_redis.clj
+++ src/jepsen/crdt_redis.clj
@@ -1,14 +1,33 @@
 (ns jepsen.crdt-redis
-  (:require [jepsen.cli :as cli]
-            [jepsen.tests :as tests]))
+  (:require [clojure.tools.logging :refer :all]
+            [clojure.string :as str]
+            [jepsen [cli :as cli]
+                    [control :as c]
+                    [db :as db]
+                    [tests :as tests]]
+            [jepsen.control.util :as cu]
+            [jepsen.os.ubuntu :as ubuntu]))
+
+(defn db
+  "CRDT-Redis for a particular version."
+  [version]
+  (reify db/DB
+    (setup! [_ test node]
+      (info node "installing crdt-redis" version))
+
+    (teardown! [_ test node]
+      (info node "tearing down crdt-redis"))))
 
 (defn crdt-redis-test
   "Given an options map from the command line runner (e.g. :nodes, :ssh,
   :concurrency, ...), constructs a test map."
   [opts]
   (merge tests/noop-test
-         {:pure-generators true}
-         opts))
+         opts
+         {:name "crdt-redis"
+          :os   ubuntu/os
+          :db   (db "v3.1.5")
+          :pure-generators true}))
 
 (defn -main
   "Handles command line arguments. Can either run a test, or a web server for
@@ -16,3 +35,5 @@
   [& args]
   (cli/run! (cli/single-test-cmd {:test-fn crdt-redis-test})
             args))
+
+;; lein run test --nodes-file nodes --password 123456 --username root
