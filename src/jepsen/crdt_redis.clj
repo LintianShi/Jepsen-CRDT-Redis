f5500b8d6f7be57eb440c51a89c7e684b2ee2bc1
LintianShi <254656131@qq.com>
2022-04-15 21:30:46 +0800

    connect redis and replicate with each other

--- src/jepsen/crdt_redis.clj
+++ src/jepsen/crdt_redis.clj
@@ -8,15 +8,43 @@
             [jepsen.control.util :as cu]
             [jepsen.os.ubuntu :as ubuntu]))
 
+(def script_path "/home/shilintian/Redis-CRDT-Experiment/experiment/redis_test/")
+(def start_server "./server.sh")
+(def shutdown_server "./shutdown.sh")
+(def clean "./clean.sh")
+(def replicate "../../redis-6.0.5/src/redis-cli")
+(def host_map {"172.24.81.136" 0 
+      "172.24.81.137" 1 
+      "172.24.81.132" 2})
+(def port "6379")
+(def local_host "127.0.0.1")
+(defn all_host_port []
+  (clojure.string/split (clojure.string/join " " (map #(str % " " port ) (keys host_map))) #" "))
+
+;; ../../redis-6.0.5/src/redis-cli -h 127.0.0.1 -p 6379 REPLICATE 3 0 AUTOMAT 172.24.81.132 6379 172.24.81.136 6379 172.24.81.137 6379
+
+(def counter (atom -1))
+
+(defn next-value []
+  (swap! counter inc))
+
 (defn db
   "CRDT-Redis for a particular version."
-  [version]
+  []
   (reify db/DB
     (setup! [_ test node]
-      (info node "installing crdt-redis" version))
+      (info node "starting crdt-redis")
+      (info (c/exec "cd" script_path c/&& start_server port))
+      (Thread/sleep 1000)
+      (info (c/exec "cd" script_path c/&& replicate "-h" local_host "-p" port "REPLICATE" (count host_map) (get host_map (str node)) "AUTOMAT" (all_host_port)))
+      (Thread/sleep 1000))
 
     (teardown! [_ test node]
-      (info node "tearing down crdt-redis"))))
+      (info node "tearing down crdt-redis")
+      (info (c/exec "cd" script_path c/&& shutdown_server port))
+      (Thread/sleep 1000)
+      (info (c/exec "cd" script_path c/&& clean port))
+      (Thread/sleep 1000))))
 
 (defn crdt-redis-test
   "Given an options map from the command line runner (e.g. :nodes, :ssh,
@@ -26,7 +54,7 @@
          opts
          {:name "crdt-redis"
           :os   ubuntu/os
-          :db   (db "v3.1.5")
+          :db   (db)
           :pure-generators true}))
 
 (defn -main
