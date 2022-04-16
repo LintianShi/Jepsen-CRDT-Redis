6773552529ec19e42cbbf02d84c06fa767036f5b
LintianShi <254656131@qq.com>
2022-04-16 17:43:23 +0800

    implement client of rwf rpq

--- project.clj
+++ project.clj
@@ -4,7 +4,8 @@
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :main jepsen.crdt-redis
   :dependencies [[org.clojure/clojure "1.10.0"]
-                 [com.taoensso/carmine "3.1.0"]
+                 [com.taoensso/carmine "3.1.0-LOCAL"]
+                 [slingshot "0.12.2"]
                  [jepsen "0.2.7-LOCAL"]]
   :repositories [["central" "https://maven.aliyun.com/nexus/content/groups/public"]
                  ["clojars" "https://mirrors.tuna.tsinghua.edu.cn/clojars/"]])
--- src/jepsen/crdt_redis.clj
+++ src/jepsen/crdt_redis.clj
@@ -8,8 +8,9 @@
                     [generator :as gen]
                     [tests :as tests]]
             [jepsen.control.util :as cu]
-            [taoensso.carmine :as car :refer (wcar)]
-            [jepsen.os.ubuntu :as ubuntu]))
+            [jepsen.os.ubuntu :as ubuntu]
+            [taoensso.carmine :as car :refer (wcar)])
+    (:use [slingshot.slingshot :only [try+]]))
 
 (def script_path "/home/shilintian/Redis-CRDT-Experiment/experiment/redis_test/")
 (def start_server "./server.sh")
@@ -51,8 +52,15 @@
 
 
 (defn r   [_ _] {:type :invoke, :f :read, :value nil})
-(defn w   [_ _] {:type :invoke, :f :write, :value (rand-int 5)})
+(defn w   [_ _] {:type :invoke, :f :write, :value (rand-int 10)})
 (defn cas [_ _] {:type :invoke, :f :cas, :value [(rand-int 5) (rand-int 5)]})
+
+(defn rwfzadd   [_ _] {:type :invoke, :f :add, :value ["rwfzadd" "default" (rand-int 5) (rand-int 100)]})
+(defn rwfzincrby   [_ _] {:type :invoke, :f :incrby, :value ["rwfzincrby" "default" (rand-int 5) (- (rand-int 200) 100)]})
+(defn rwfzrem   [_ _] {:type :invoke, :f :rem, :value ["rwfzrem" "default" (rand-int 5)]})
+(defn rwfzscore   [_ _] {:type :invoke, :f :score, :value ["rwfzscore" "default" (rand-int 5)]})
+(defn rwfzmax   [_ _] {:type :invoke, :f :max, :value ["rwfzmax" "default"]})
+
 
 (defmacro wcar* [node & body] `(car/wcar {:pool {:host node :port 6379} :spec {}} ~@body))
 (def tkey (partial car/key :carmine :temp :test))
@@ -80,6 +88,34 @@
 
   (close! [_ test]))
 
+(defrecord RwfPQClient [conn]
+  client/Client
+  (open! [this test node]
+    (assoc this :conn node))
+
+  (setup! [this test])
+
+  (invoke! [_ test op]
+    (case (:f op)
+        :add (try+ (do (car/wcar {:pool {} :spec {:host conn :port 6379}} (car/redis-call (:value op)))
+                 (assoc op :type :ok))
+              (catch [] ex
+                  (assoc op :type :ok)))
+        :incrby (try+ (do (car/wcar {:pool {} :spec {:host conn :port 6379}} (car/redis-call (:value op)))
+                    (assoc op :type :ok))
+                (catch [] ex
+                  (assoc op :type :ok)))
+        :rem (try+ (do (car/wcar {:pool {} :spec {:host conn :port 6379}} (car/redis-call (:value op)))
+                 (assoc op :type :ok))
+              (catch [] ex
+                  (assoc op :type :ok)))
+        :score (assoc op :type :ok, :value (car/wcar {:pool {} :spec {:host conn :port 6379}} (car/redis-call (:value op))))
+        :max (assoc op :type :ok, :value (car/wcar {:pool {} :spec {:host conn :port 6379}} (car/redis-call (:value op))))))
+
+  (teardown! [this test])
+
+  (close! [_ test]))
+
 
 
 (defn crdt-redis-test
@@ -92,11 +128,11 @@
           :os   ubuntu/os
           :db   (db)
           :pure-generators true
-          :client (Client. nil)
-          :generator       (->> (gen/mix [r w])
+          :client (RwfPQClient. nil)
+          :generator       (->> (gen/mix [rwfzadd rwfzincrby rwfzrem rwfzscore rwfzmax])
                                 (gen/stagger 1)
                                 (gen/nemesis nil)
-                                (gen/time-limit 15))}))
+                                (gen/time-limit 30))}))
 
 (defn -main
   "Handles command line arguments. Can either run a test, or a web server for
